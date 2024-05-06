973f22b133b7f6ecfaf5a9810b0d1f3ab01845b2
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2024-05-06 09:10:23 +0700

    feat/edit_course: test edit course function

--- Project3_software_testing/main.py
+++ Project3_software_testing/main.py
@@ -1,11 +1,16 @@
 import unittest
+import sys
 
+sys.path.append('./editcourse')
 # Khởi tạo TestLoader
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # Thêm các test vào TestSuite
-suite.addTests(loader.discover('./login'))
+#suite.addTests(loader.discover('../Project3_software_testing'))
+#suite.addTests(loader.discover('./login'))
+suite.addTests(loader.discover('./editcourse'))
+
 
 
 # Khởi tạo TestRunner và chạy TestSuite
