1af5f696144c971d521d3cd227813a73f54b60ea
tanNguyen2220022 <147414456+tanNguyen2220022@users.noreply.github.com>
2024-05-11 12:08:10 +0700

    Update main.py to add 4 new folders

--- Project3_software_testing/main.py
+++ Project3_software_testing/main.py
@@ -11,6 +11,10 @@ suite.addTests(loader.discover('../Project3_software_testing'))
 #suite.addTests(loader.discover('./login'))
 #suite.addTests(loader.discover('./edit_course'))
 #suite.addTests(loader.discover('./search_student'))
+# suite.addTests(loader.discover('./2220022_edit_quiz_time_limit'))
+# suite.addTests(loader.discover('./2220022_upload_file_txt'))
+# suite.addTests(loader.discover('./2220022_view_quizz_result_tf'))
+# suite.addTests(loader.discover('./222002_security_access'))
 
 
 
